7fec35998906621e43ea68a782ce665c59d5befa
ayuba <olanrewajuayuba@gmail.com>
2020-03-25 02:35:27 +0100

    first commit

--- .history/src/App_20200324010547.js
+++ .history/src/App_20200324010547.js
@@ -11,7 +11,7 @@ function App() {
     results: [],
     selected: {}
   });
-  const apiurl = "http://www.omdbapi.com/?apikey=1ee0ea1b";
+  const apiurl = "http://www.omdbapi.com/?apikey=dfe6d885";
 
   const search = (e) => {
     if (e.key === "Enter") {
