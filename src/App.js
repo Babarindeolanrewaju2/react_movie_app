57e5c9375f9a921e91a2b97a2d3d38456a11b680
Babarindeolanrewaju2 <49330610+Babarindeolanrewaju2@users.noreply.github.com>
2020-10-24 00:03:06 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     results: [],
     selected: {}
   });
-  const apiurl = "http://www.omdbapi.com/?apikey=aef371f7";
+  const apiurl = "https://www.omdbapi.com/?apikey=aef371f7";
 
   const search = (e) => {
     if (e.key === "Enter") {
